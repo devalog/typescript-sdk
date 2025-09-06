f1353afdd764dd7db554bb155084c63cba948cfa
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-09-06 17:56:15 +0000

    SDK regeneration

--- src/Client.ts
+++ src/Client.ts
@@ -40,8 +40,8 @@ export class MyClientNameDevinClient {
                 {
                     "X-Fern-Language": "JavaScript",
                     "X-Fern-SDK-Name": "devin-package-new",
-                    "X-Fern-SDK-Version": "0.1.3",
-                    "User-Agent": "devin-package-new/0.1.3",
+                    "X-Fern-SDK-Version": "0.1.4",
+                    "User-Agent": "devin-package-new/0.1.4",
                     "X-Fern-Runtime": core.RUNTIME.type,
                     "X-Fern-Runtime-Version": core.RUNTIME.version,
                 },
